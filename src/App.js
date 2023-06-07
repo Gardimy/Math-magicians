2bbf8ab244e1d9b9110b0a22c9e746190fb93f2b
Gardimy Charles <gardimycharles889@gmail.com>
2023-06-07 19:44:13 -0400

    Add Calculator.js and calculator.css files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import React from 'react';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> You can Calculate now!</h1>
-      </header>
+      <Calculator />
     </div>
   );
 }
